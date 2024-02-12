bd7a80b5f67d076aee1ca0ea498445a5c59a1b2d
thiagovieiras <thiago.santos@sptech.school>
2024-02-12 16:16:18 -0300

    metodo de cadastro do usuario feito

--- src/main/java/com/example/projetoplanta/com/example/projetoplanta/controllers/UsuarioController.java
+++ src/main/java/com/example/projetoplanta/com/example/projetoplanta/controllers/UsuarioController.java
@@ -1,15 +1,31 @@
 package com.example.projetoplanta.com.example.projetoplanta.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.projetoplanta.com.example.projetoplanta.DTO.UsuarioRecordDTO;
+import com.example.projetoplanta.com.example.projetoplanta.modules.UsuarioModel;
 import com.example.projetoplanta.com.example.projetoplanta.repositories.UsuarioRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UsuarioController {
-    
+
     @Autowired
     UsuarioRepository usuarioRepository;
+
+    @PostMapping("/usuario")
+    public ResponseEntity<UsuarioModel> cadastrarUsuario(@RequestBody @Valid UsuarioRecordDTO usuario) {
+        var usuarioModel = new UsuarioModel();
+        BeanUtils.copyProperties(usuario, usuarioModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuarioModel));
+    }
 
     
 }
