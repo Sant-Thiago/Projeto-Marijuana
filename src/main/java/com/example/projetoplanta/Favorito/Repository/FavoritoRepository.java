2ae1080b782390477d000e64bcd1a65b4688ab54
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-07-06 18:05:11 -0300

    arrumando o repository do favorito

--- src/main/java/com/example/projetoplanta/Favorito/Repository/FavoritoRepository.java
+++ src/main/java/com/example/projetoplanta/Favorito/Repository/FavoritoRepository.java
@@ -3,13 +3,17 @@ package com.example.projetoplanta.Favorito.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.projetoplanta.Favorito.Module.FavoritoModel;
 
 @Repository
 public interface FavoritoRepository extends JpaRepository<FavoritoModel, Integer> {
+    @Query("SELECT f FROM favorito FavoritoModel WHERE f.fkUsuario.id = :fkUsuario")
+    List<FavoritoModel> findByFkUsuario(@Param("fkUsuario")String fkUsuario);
 
-    List<FavoritoModel> findByFkUsuario(String fkUsuario);
-    List<FavoritoModel> findByFkPlanta(String fkPlanta);
+    @Query("SELECT f FROM favorito FavoritoModel WHERE f.fkPlanta.id = :fkPlanta")
+    List<FavoritoModel> findByFkPlanta(@Param("fkPlanta") String fkPlanta);
 }
